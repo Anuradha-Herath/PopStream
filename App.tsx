b4e40dc2dd56eee927e1f1dc81ebf4a43db0aa46
Anuradha Herath <anuradhaherath2001@gmail.com>
2025-11-11 20:03:41 +0530

    Fix app loading by hiding splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/redux/store';
-import RootNavigator from './src/navigation/RootNavigator';
+import React, { useEffect } from "react";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./src/redux/store";
+import RootNavigator from "./src/navigation/RootNavigator";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
